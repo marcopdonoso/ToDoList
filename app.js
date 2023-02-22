5d4770814784896fb0d23f014b21f1a5e473cdd6
Marco Perez Donoso <marcopdonoso@gmail.com>
2023-02-22 11:57:34 -0400

    delete items from todo list database

--- app.js
+++ app.js
@@ -70,6 +70,18 @@ app.post("/", function(req, res){
   res.redirect("/");
 });
 
+app.post("/delete", function(req, res) {
+  const checkedItemId = req.body.checkbox;
+  Item.findByIdAndDelete(checkedItemId, function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Succesfully deleted item");
+      res.redirect("/");
+    }
+  });
+});
+
 app.get("/work", function(req,res){
   res.render("list", {listTitle: "Work List", newListItems: workItems});
 });
