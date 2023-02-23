6e922b205a7c06a3350ea153eae6feb9bec0992a
Marco Perez Donoso <marcopdonoso@gmail.com>
2023-02-23 16:00:43 -0400

    Change listening port to automatic

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const _ = require("lodash");
 
+const PORT = process.env.PORT || 3000;
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -144,6 +146,6 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(PORT, function() {
+  console.log("Server started on port " + PORT);
 });
